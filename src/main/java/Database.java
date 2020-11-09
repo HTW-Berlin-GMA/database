eda4d3924772771460a2c45a599462b9e8e2c429
Dustin Eikmeier <s0569494@htw-berlin.de>
2020-11-09 12:18:52 +0100

    init

---
+++ src/main/java/Database.java
@@ -0,0 +1,8 @@
+/**
+ * @author Dustin Eikmeier
+ * @version 1.0
+ * @since 1.8
+ */
+
+public interface Database {
+}
---
+++ src/main/java/databse/Database.java
@@ -0,0 +1,25 @@
+/**
+ * @author Dustin Eikmeier
+ * @version 1.0
+ * @since 1.8
+ */
+
+package databse;
+
+import net.sf.jdbcwrappers.DataSourceWrapper;
+import net.sf.jdbcwrappers.log.Logger;
+import net.sf.jdbcwrappers.log.LoggingConfiguration;
+import net.sf.jdbcwrappers.log.LoggingConnectionWrapper;
+import org.mockserver.examples.mockserver.initializer.ExpectationInitializerExample;
+import org.mockserver.logging.MockServerLogger;
+import org.mockserver.server.initialize.ExpectationInitializerLoader;
+
+public class Database {
+    //private org.slf4j.Logger log = new
+    private org.mockserver.logging.MockServerLogger mockServerLogger = new MockServerLogger();
+    //private org.mockserver.server.initialize.ExpectationInitializerLoader expectationInitializerLoader = new ExpectationInitializerLoader();
+
+    public void method(){
+
+    }
+}
